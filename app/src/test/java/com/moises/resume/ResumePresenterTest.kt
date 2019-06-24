d1cfb709422d39f4300c1fdcd06fb5aaf979177a
Moisés Garduño Reyes <moises.garduno.r@gmail.com>
2019-06-24 15:15:42 -0500

    Implement mockito in kotlin

--- app/src/test/java/com/moises/resume/ResumePresenterTest.kt
+++ app/src/test/java/com/moises/resume/ResumePresenterTest.kt
@@ -1,32 +1,27 @@
 package com.moises.resume
 
 import com.moises.data.model.Profile
-import com.moises.data.usecase.ResumeUseCase
 import com.moises.presentation.resume.ResumePresenter
-import com.moises.presentation.resume.ResumePresenterImpl
 import com.moises.presentation.resume.ResumeView
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.never
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 
 
 class ResumePresenterTest {
 
-    lateinit var resumeUseCase: ResumeUseCase
-    lateinit var resumeView : ResumeView
+    @Mock
     lateinit var resumePresenter: ResumePresenter
+    @Mock
+    lateinit var resumeView: ResumeView
+    @Mock
     lateinit var profile : Profile
 
     @Before
     fun setup() {
-        resumeUseCase = Mockito.mock(ResumeUseCase::class.java)
-        resumeView = Mockito.mock(ResumeView::class.java)
-        profile = Mockito.mock(Profile::class.java)
-        resumePresenter = ResumePresenterImpl(resumeUseCase, resumeView)
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
